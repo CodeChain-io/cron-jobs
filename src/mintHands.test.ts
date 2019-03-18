68d2ca58fe006c9a63f418263e2a8bd7ecdb432a
Seulgi Kim <seulgi.kim@kodebox.io>
2019-03-18 13:06:15 +0900

    Create clock hands

---
+++ src/mintHands.test.ts
@@ -0,0 +1,85 @@
+import { expect } from "chai";
+import { AssetTransferAddress } from "codechain-primitives";
+import { SDK } from "codechain-sdk";
+import { KeyStore } from "codechain-sdk/lib/key/KeyStore";
+import * as config from "config";
+import "mocha";
+import { tmpdir } from "os";
+import * as randomstring from "randomstring";
+import { mintHands } from "./mintHands";
+
+const networkId = config.get<string>("network_id");
+
+export const faucetSecret =
+    "ede1d4ccb4ec9a8bbbae9a13db3f4a7b56ea04189be86ac3a6a439d9a0a1addd";
+export const faucetAccointId = SDK.util.getAccountIdFromPrivate(faucetSecret); // 6fe64ffa3a46c074226457c90ccb32dc06ccced1
+export const faucetAddress = SDK.Core.classes.PlatformAddress.fromAccountId(
+    faucetAccointId,
+    { networkId }
+); // tccq9h7vnl68frvqapzv3tujrxtxtwqdnxw6yamrrgd
+
+const dbPath = `${tmpdir()}/${randomstring.generate({
+    length: 12,
+    charset: "alphabetic"
+})}`;
+
+const rpcUrl = config.get<string>("rpc_url");
+const sdk = new SDK({ server: rpcUrl, networkId });
+
+describe("Clock hands", async function() {
+    let keyStore: KeyStore;
+    let payer: string;
+    const passphrase = "pass";
+    let users: string[];
+
+    before(async function() {
+        keyStore = await sdk.key.createLocalKeyStore(dbPath);
+        payer = (await sdk.key.createPlatformAddress({ keyStore, passphrase }))
+            .value;
+
+        const seq = await sdk.rpc.chain.getSeq(faucetAddress);
+        const pay = sdk.core
+            .createPayTransaction({ recipient: payer, quantity: 1000 })
+            .sign({ secret: faucetSecret, seq, fee: 10 });
+        await sdk.rpc.chain.sendSignedTransaction(pay);
+
+        users = [];
+        this.timeout(60_000);
+        for (let i = 0; i < 60; i += 1) {
+            const user = await sdk.key.createAssetTransferAddress({
+                type: "P2PKH",
+                passphrase
+            });
+            users.push(user.value);
+        }
+    });
+
+    it("Check owners", async function() {
+        const date = new Date();
+        const [hour, minute, second] = mintHands(sdk, users, date, payer);
+
+        const hourOwner = Buffer.from(
+            AssetTransferAddress.fromString(
+                users[date.getUTCHours()]
+            ).payload.toString(),
+            "hex"
+        );
+        expect(hour.getMintedAsset().parameters).deep.equal([hourOwner]);
+
+        const minuteOwner = Buffer.from(
+            AssetTransferAddress.fromString(
+                users[date.getUTCMinutes()]
+            ).payload.toString(),
+            "hex"
+        );
+        expect(minute.getMintedAsset().parameters).deep.equal([minuteOwner]);
+
+        const secondOwner = Buffer.from(
+            AssetTransferAddress.fromString(
+                users[date.getUTCSeconds()]
+            ).payload.toString(),
+            "hex"
+        );
+        expect(second.getMintedAsset().parameters).deep.equal([secondOwner]);
+    });
+});
---
+++ src/mintHands.ts
@@ -0,0 +1,61 @@
+import { PlatformAddress } from "codechain-primitives";
+import { SDK } from "codechain-sdk";
+import { MintAsset } from "codechain-sdk/lib/core/transaction/MintAsset";
+
+export function mintHands(
+    sdk: SDK,
+    users: string[],
+    date: Date,
+    approver: string
+): [MintAsset, MintAsset, MintAsset] {
+    const hourHand = sdk.core.createAssetScheme({
+        shardId: 0,
+        metadata: JSON.stringify({
+            name: "Hour Hand",
+            description: `An hour hand of ${approver}'s clock`,
+            icon_url:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Clock-hour-10.svg/200px-Clock-hour-10.svg.png"
+        }),
+        approver: PlatformAddress.fromString(approver),
+        supply: 1
+    });
+
+    const minuteHand = sdk.core.createAssetScheme({
+        shardId: 0,
+        metadata: JSON.stringify({
+            name: "Minute Hand",
+            description: `A minute hand of ${approver}'s clock`,
+            icon_url:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Clock-minute-15.svg/200px-Clock-minute-15.svg.png"
+        }),
+        approver: PlatformAddress.fromString(approver),
+        supply: 1
+    });
+
+    const secondHand = sdk.core.createAssetScheme({
+        shardId: 0,
+        metadata: JSON.stringify({
+            name: "Second Hand",
+            description: `A second hand of ${approver}'s clock`,
+            icon_url:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Red_Second_Hand_from_a_Clockjpg.jpeg/320px-Red_Second_Hand_from_a_Clockjpg.jpeg"
+        }),
+        approver: PlatformAddress.fromString(approver),
+        supply: 1
+    });
+
+    return [
+        sdk.core.createMintAssetTransaction({
+            scheme: hourHand,
+            recipient: users[date.getUTCHours()]
+        }),
+        sdk.core.createMintAssetTransaction({
+            scheme: minuteHand,
+            recipient: users[date.getUTCMinutes()]
+        }),
+        sdk.core.createMintAssetTransaction({
+            scheme: secondHand,
+            recipient: users[date.getUTCSeconds()]
+        })
+    ];
+}
