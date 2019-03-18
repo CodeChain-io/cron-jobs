467129a03b7c627264681dec9373c5228e06f80e
Seulgi Kim <seulgi.kim@kodebox.io>
2019-03-18 14:27:42 +0900

    Create 60 users and store the list in a file

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
 import { SDK } from "codechain-sdk";
 import * as config from "config";
+import { createUsers, loadUsers, storeUsers } from "./src/users";
 
 if (require.main === module) {
     const rpcUrl = config.get<string>("rpc_url")!;
     const networkId = config.get<string>("network_id")!;
+    const passphrase = config.get<string>("passphrase");
     const sdk = new SDK({ server: rpcUrl, networkId });
+    (async () => {
+        const users = await loadUsers(".users").catch((err: Error) => {
+            console.error(err.message);
+            return createUsers(sdk, passphrase).then(createdUsers => {
+                storeUsers(".users", createdUsers);
+                return createdUsers;
+            });
+        });
+    })().catch(console.error);
 }
---
+++ src/users.ts
@@ -0,0 +1,66 @@
+import { SDK } from "codechain-sdk";
+import * as fs from "fs";
+
+export function loadUsers(filename: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        if (!fs.existsSync(filename)) {
+            reject(Error(`${filename} not exists.`));
+            return;
+        }
+
+        fs.readFile(filename, { encoding: "utf8" }, (err, data) => {
+            if (err != null) {
+                reject(err);
+                return;
+            }
+            const users = JSON.parse(data);
+            if (users.length !== 60) {
+                reject(
+                    Error(
+                        `There are ${
+                            users.length
+                        } users. The file may be corrupted.`
+                    )
+                );
+                return;
+            }
+            resolve(users);
+        });
+    });
+}
+
+export async function createUsers(
+    sdk: SDK,
+    passphrase?: string
+): Promise<string[]> {
+    const users = [];
+    for (let i = 0; i < 60; i += 1) {
+        const user = await sdk.key.createAssetTransferAddress({
+            type: "P2PKH",
+            passphrase
+        });
+        users.push(user.value);
+    }
+    return users;
+}
+
+export function storeUsers(filename: string, users: string[]): Promise<null> {
+    if (fs.existsSync(filename)) {
+        throw Error(`${filename} already exists.`);
+    }
+
+    return new Promise((resolve, reject) => {
+        fs.writeFile(
+            filename,
+            JSON.stringify(users),
+            { encoding: "utf-8", mode: 0o600 },
+            err => {
+                if (err != null) {
+                    reject(err);
+                    return;
+                }
+                resolve(null);
+            }
+        );
+    });
+}
