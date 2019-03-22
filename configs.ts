4be7f12b05e2d6b5ce2f2b42a08845175eb2c80b
SeongChan Lee <sclee@kodebox.io>
2019-03-22 18:26:55 +0900

    Disable some accounts

--- configs.ts
+++ configs.ts
@@ -48,12 +48,11 @@ export const ACCOUNTS: PlatformAddress[] = [
     "61e81175e52e0cd519e72d5afbe08a65eadd38da", // tccq9s7syt4u5hqe4geuuk447lq3fj74hfcmgm9vv08
     "69845440b69b324fb560879866d539a482c0e3b8", // tccq95cg4zqk6dnyna4vzresek48xjg9s8rhq67cvt7
     "d2ac58696662c08ee37b2d9e5ac3bde3b14f8dfe", // tccq8f2ckrfve3vprhr0vkeukkrhh3mznudlckp6kqq
-    "457f3c295de54a1044197563c47a20347daa0a23", // tccq9zh70pfthj55yzyr96k83r6yq68m2s2yv8dqgxp
-    "ecc6aba091aa62a56bda65b7ec270b55d5798175", // tccq8kvd2aqjx4x9fttmfjm0mp8pd2a27vpw53j2jhh
-    "67a88eab5e26be65a4c9671fa445caeaacd78e72", // tccq9n63r4ttcntuedye9n3lfz9et42e4uwwged7vhp
-    "e419a134b85f941416edec82d7bafbfe837c9ab2", // tccq8jpngf5hp0eg9qkahkg94a6l0lgxly6kgks2a25
-    "50c9360a5ae6b4e1c98fd61924aeb86e56f085e2", // tccq9gvjds2ttntfcwf3ltpjf9whph9duy9ug4tk64r
-    "fecc099b610ca5c3feec6dd5c53de9d0b20936b9", // tccq8lvczvmvyx2tsl7a3kat3faa8gtyzfkhycx29ls
+    // "457f3c295de54a1044197563c47a20347daa0a23", // tccq9zh70pfthj55yzyr96k83r6yq68m2s2yv8dqgxp
+    // "ecc6aba091aa62a56bda65b7ec270b55d5798175", // tccq8kvd2aqjx4x9fttmfjm0mp8pd2a27vpw53j2jhh
+    // "67a88eab5e26be65a4c9671fa445caeaacd78e72", // tccq9n63r4ttcntuedye9n3lfz9et42e4uwwged7vhp
+    // "e419a134b85f941416edec82d7bafbfe837c9ab2", // tccq8jpngf5hp0eg9qkahkg94a6l0lgxly6kgks2a25
+    // "50c9360a5ae6b4e1c98fd61924aeb86e56f085e2", // tccq9gvjds2ttntfcwf3ltpjf9whph9duy9ug4tk64r
 ].map(accountId => PlatformAddress.fromAccountId(accountId, { networkId: sdk.networkId }));
 
 export const ASSET_SCHEMES: AssetScheme[] = [
