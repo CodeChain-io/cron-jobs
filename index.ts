c1aeaec73fb60522e563110e5ab5c33d9892d7e3
Park Juhyung <jh@kodebox.io>
2019-03-26 12:18:36 +0900

    Add exponential backoff

--- index.ts
+++ index.ts
@@ -3,17 +3,27 @@ import { runWaitUntilTimelock } from "./src/scenario";
 import { SlackNotification } from "./src/slackNotification";
 import { delay } from "./src/util";
 
+const MAX_ERROR_COUNT = 4;
+
 async function main() {
     const codeChain = new CodeChain();
+    let errorCount = 0;
     while (true) {
         try {
             await runWaitUntilTimelock(codeChain);
+            errorCount = 0;
         } catch (err) {
             console.error(err);
             SlackNotification.instance.sendError(err);
+
+            errorCount += 1;
+            if (errorCount > MAX_ERROR_COUNT) {
+                errorCount = MAX_ERROR_COUNT;
+            }
         }
 
-        await delay(3 * 1000);
+        const delaySeconds = Math.pow(10, errorCount);
+        await delay(delaySeconds * 1000);
     }
 }
 
