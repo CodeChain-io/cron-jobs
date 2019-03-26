9fe413ae47ddc6994e754fe65e8ca27215f59be1
Park Juhyung <jh@kodebox.io>
2019-03-26 12:11:22 +0900

    Stop noop transactions when error occurred

--- src/scenario/waitUntilTimeLock.ts
+++ src/scenario/waitUntilTimeLock.ts
@@ -5,48 +5,57 @@ import { delay } from "../util";
 let emptyTransaction: boolean = false;
 
 export async function run(codeChain: CodeChain) {
-    emptyTransaction = true;
-    await codeChain.fillMoneyForNoop();
-    runEmptyTransaction(codeChain);
+    try {
+        emptyTransaction = true;
+        await codeChain.fillMoneyForNoop();
+        runEmptyTransaction(codeChain);
 
-    const startBlockHeight = await codeChain.getCurrentBlockHeight();
-    console.log(`StartBlockHeight: ${startBlockHeight}`);
-    const utxoSet = await codeChain.prepareUTXOs();
+        const startBlockHeight = await codeChain.getCurrentBlockHeight();
+        console.log(`StartBlockHeight: ${startBlockHeight}`);
+        const utxoSet = await codeChain.prepareUTXOs();
 
-    const transaction = await codeChain.createTimeLockTransaction({
-        input: utxoSet.popAsset(),
-        timelock: {
-            type: "block",
-            value: startBlockHeight + 10
+        const transaction = await codeChain.createTimeLockTransaction({
+            input: utxoSet.popAsset(),
+            timelock: {
+                type: "block",
+                value: startBlockHeight + 10
+            }
+        });
+        await codeChain.sendTransaction(transaction);
+        const leastBlock = await codeChain.waitFutureBlock({
+            canHandle: transaction
+        });
+
+        chai.assert.isAtLeast(leastBlock.number, startBlockHeight + 10);
+
+        const result = await codeChain.getResult(transaction);
+        if (result === null) {
+            throw new Error(
+                "The transaction is not processed after 60 seconds"
+            );
         }
-    });
-    await codeChain.sendTransaction(transaction);
-    const leastBlock = await codeChain.waitFutureBlock({
-        canHandle: transaction
-    });
 
-    chai.assert.isAtLeast(leastBlock.number, startBlockHeight + 10);
+        if (result === false) {
+            throw new Error("TimeLockTransaction has failed");
+        }
 
-    const result = await codeChain.getResult(transaction);
-    if (result === null) {
-        throw new Error("The transaction is not processed after 60 seconds");
+        const block = await codeChain.getBlockOfTransaction(transaction);
+        if (block.number < leastBlock.number) {
+            throw new Error(
+                "TimeLockTransaction executed before the time lock"
+            );
+        }
+
+        console.log("Timelock success");
+        console.log(`StartBlockHeight: ${startBlockHeight}`);
+        console.log(`leastBlockHeight: ${leastBlock.number}`);
+        console.log(`minedBlockHeight: ${block.number}`);
+
+        emptyTransaction = false;
+    } catch (err) {
+        emptyTransaction = false;
+        throw err;
     }
-
-    if (result === false) {
-        throw new Error("TimeLockTransaction has failed");
-    }
-
-    const block = await codeChain.getBlockOfTransaction(transaction);
-    if (block.number < leastBlock.number) {
-        throw new Error("TimeLockTransaction executed before the time lock");
-    }
-
-    console.log("Timelock success");
-    console.log(`StartBlockHeight: ${startBlockHeight}`);
-    console.log(`leastBlockHeight: ${leastBlock.number}`);
-    console.log(`minedBlockHeight: ${block.number}`);
-
-    emptyTransaction = false;
 }
 
 async function runEmptyTransaction(codeChain: CodeChain) {
