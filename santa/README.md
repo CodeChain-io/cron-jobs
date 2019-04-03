f754d9616fbc49ec7688674f799c1c1d95359b00
Seonpyo Kim <tomandjerry@kodebox.io>
2019-04-03 18:20:51 +0900

    Replant Santa

---
+++ santa/README.md
@@ -0,0 +1,45 @@
+# CodeChain Santa
+
+This project is aimed to airdrop AssetTransferTransactions, including orders for CodeChain.
+
+## How to run
+- create a configuration file under the `config` folder.
+- run the command 
+  ```
+    $ yarn install
+    $ yarn start
+  ```
+
+## What Santa needs
+- Santa needs a platform address.
+  - This address is the payer so it should have enough CCC to execute transactions.
+- Santa needs a CodeChain node to use the RPC.
+
+## What Santa creates
+- Santa creates 10 asset addresses.
+  - They will be participants of `AssetTransferTransactions` including `Orders`.
+- Santa mints 20 assets.
+  - Each asset address will have two assets: One of them will be used to trade with other assets, and
+    the other will be used to pay fees for orders.
+- Santa creates regular key for the faucet address.
+   - Initially a regular key is created.
+   - There is a 2% chance every iteration for the regular key to be renewed. 
+- Santa creates and mints every time it is initialized.
+
+## What Santa does
+- Every 10 seconds, Santa first generates `Orders`.
+  - These orders may or may not include fees.
+  - Trading assets will be chosen randomly.
+  - Trading ratios are determined randomly.
+  - There is about a 45% chance to generate a single order and a 45% chance to generate two related orders.
+  - There is about a 10% chance to generate highly(3 or more) entangled orders.
+  - Generated Orders may either be entangled with each other, or only a single generated order.
+- Using generated `Orders`, Santa generates `AssetTransferTransaction`.
+  - The Generated transaction has consistent inputs and outputs with orders.
+  - The trading quantities are randomly determined among the multiples of the given ratios in `Orders`.
+  - There is about a 50% chance to generate a transaction which continuously fills the previous partially filled order.
+- Using FlawGenerator, Santa contaminates `Order` and `AssetTransferTransaction`.
+  - Here, the term 'contaminate' means Santa injects errors to make them incorrect.   
+  - There is a 10% chance each for `Order` and `AssetTranferTransaction` to be contaminated.
+  - Then those contaminated `Order` and `AssetTranferTransaction` are transmitted to the SDK.
+  - Errors would be detected during the importation.
---
+++ santa/src/FlawGenerator.ts
@@ -0,0 +1,188 @@
+import { Order } from "codechain-sdk/lib/core/transaction/Order";
+import { TransferAsset } from "codechain-sdk/lib/core/transaction/TransferAsset";
+import { AssetManager } from "./AssetManager";
+import Helper, { randRange } from "./util";
+
+export class FlawGenerator {
+    private helper: Helper;
+    private assetManager: AssetManager;
+
+    constructor(helper: Helper, assetManager: AssetManager) {
+        this.helper = helper;
+        this.assetManager = assetManager;
+    }
+
+    public polluteOrder(targetOrder: Order): Order {
+        const methodList = [
+            this.polluteAssetType,
+            this.polluteQuantityFee,
+            this.polluteRecipient,
+            this.polluteOriginOutput
+        ];
+        const selected = methodList[randRange(0, methodList.length - 1)];
+        return selected.bind(this)(targetOrder);
+    }
+
+    public polluteTransaction(targetTransaction: TransferAsset): TransferAsset {
+        const methodList = [
+            this.polluteSpentQuantity,
+            this.polluteInputIndices,
+            this.polluteOutputIndices,
+            this.polluteInputs,
+            this.polluteOutputs
+        ];
+        const selected = methodList[randRange(0, methodList.length - 1)];
+        return selected.bind(this)(targetTransaction);
+    }
+
+    private polluteAssetType(target: Order): Order {
+        const { assetTypeFrom, assetTypeTo, assetTypeFee, ...rest } = target;
+
+        let newAssetTypeFrom;
+        let newAssetTypeTo;
+        let newAssetTypeFee;
+
+        if (!rest.assetQuantityFee.eq(0)) {
+            newAssetTypeFrom = assetTypeFrom;
+            newAssetTypeTo = assetTypeTo;
+            newAssetTypeFee = assetTypeFrom;
+        } else {
+            newAssetTypeFrom = assetTypeFrom;
+            newAssetTypeTo = assetTypeFrom;
+        }
+        return this.helper.sdk.core.createOrder({
+            assetTypeFrom: newAssetTypeFrom,
+            assetTypeTo: newAssetTypeTo,
+            assetTypeFee: newAssetTypeFee,
+            ...rest
+        });
+    }
+
+    private polluteQuantityFee(target: Order): Order {
+        const { assetQuantityFee, ...rest } = target;
+
+        const newAssetQuantityFee = assetQuantityFee.plus(1);
+        return this.helper.sdk.core.createOrder({
+            assetQuantityFee: newAssetQuantityFee,
+            ...rest
+        });
+    }
+
+    private polluteRecipient(target: Order): Order {
+        const {
+            lockScriptHashFrom,
+            lockScriptHashFee,
+            parametersFrom,
+            parametersFee,
+            assetQuantityFee,
+            ...rest
+        } = target;
+
+        const newAssetQuantityFee = assetQuantityFee.eq(0)
+            ? assetQuantityFee.plus(1)
+            : assetQuantityFee;
+        return this.helper.sdk.core.createOrder({
+            lockScriptHashFrom,
+            lockScriptHashFee: lockScriptHashFrom,
+            parametersFrom,
+            parametersFee: parametersFrom,
+            assetQuantityFee: newAssetQuantityFee,
+            ...rest
+        });
+    }
+
+    private polluteOriginOutput(target: Order): Order {
+        const { originOutputs, ...rest } = target;
+
+        const wallets = this.assetManager.wallets;
+        const idxBox = this.assetManager.idxBox;
+
+        originOutputs.push(
+            wallets[idxBox[idxBox.length - 1]].feeAsset.outPoint
+        );
+        return this.helper.sdk.core.createOrder({
+            originOutputs,
+            ...rest
+        });
+    }
+
+    private polluteSpentQuantity(target: TransferAsset): TransferAsset {
+        const result = this.helper.sdk.core.createTransferAssetTransaction();
+        const inputs = target.inputs();
+        const outputs = target.outputs();
+
+        const { spentQuantity, ...rest } = target.orders()[0];
+        const newSpentQuantity =
+            Math.random() < 0.5
+                ? spentQuantity.plus(1)
+                : spentQuantity.times(100);
+
+        result
+            .addInputs(inputs)
+            .addOutputs(outputs)
+            .addOrder({
+                spentQuantity: newSpentQuantity,
+                ...rest
+            });
+
+        for (const order of target.orders().slice(1)) {
+            result.addOrder(order);
+        }
+
+        return result;
+    }
+
+    private polluteInputIndices(target: TransferAsset): TransferAsset {
+        const result = this.helper.sdk.core.createTransferAssetTransaction();
+        const inputs = target.inputs();
+        const outputs = target.outputs();
+
+        const { inputIndices, ...rest } = target.orders()[0];
+        inputIndices.push(0);
+
+        result
+            .addInputs(inputs)
+            .addOutputs(outputs)
+            .addOrder({
+                inputIndices,
+                ...rest
+            });
+
+        for (const order of target.orders().slice(1)) {
+            result.addOrder(order);
+        }
+
+        return result;
+    }
+
+    private polluteOutputIndices(target: TransferAsset): TransferAsset {
+        const result = this.helper.sdk.core.createTransferAssetTransaction();
+        const inputs = target.inputs();
+        const outputs = target.outputs();
+
+        const { outputIndices, ...rest } = target.orders()[0];
+        outputIndices.push(0);
+
+        result
+            .addInputs(inputs)
+            .addOutputs(outputs)
+            .addOrder({
+                outputIndices,
+                ...rest
+            });
+
+        for (const order of target.orders().slice(1)) {
+            result.addOrder(order);
+        }
+
+        return result;
+    }
+
+    private polluteInputs(target: TransferAsset): TransferAsset {
+        return target.addInputs(target.input(0)!);
+    }
+
+    private polluteOutputs(target: TransferAsset): TransferAsset {
+        return target.addOutputs(target.output(0)!);
+    }
+}
