1845f9caf0bc8c89b2ad63143de4abda5adffbd4
Seulgi Kim <seulgi.kim@codechain.io>
2019-04-30 20:53:29 +0900

    [monitor] Add level prefix to notifications

--- monitor/Alert.ts
+++ monitor/Alert.ts
@@ -2,19 +2,21 @@ import { U64 } from "codechain-primitives";
 import { getConfig } from "./util";
 
 const networkId = getConfig<string>("network_id");
-const prefix = `[${networkId} network]`;
 
 export interface CodeChainAlert {
   readonly title: string;
   readonly content: string;
+  readonly level: "error" | "warn" | "info";
 }
 
 export class CodeChainDeath implements CodeChainAlert {
   public readonly title: string;
   public readonly content: string;
+  public readonly level = "error";
 
   constructor() {
     const suffix = `${new Date().toISOString()}`;
+    const prefix = `[${this.level}][${networkId}][monitor]`;
     this.title = `${prefix} CodeChain Death Confirmation ${suffix}`;
     this.content = "CodeChain didn't renew the best block number for 1 hour.";
   }
@@ -23,9 +25,11 @@ export class CodeChainDeath implements CodeChainAlert {
 export class ViewTooHigh implements CodeChainAlert {
   public readonly title: string;
   public readonly content: string;
+  public readonly level = "warn";
 
   constructor(blockNumber: number, view: U64) {
     const suffix = `${new Date().toISOString()}`;
+    const prefix = `[${this.level}][${networkId}][monitor]`;
     this.title = `${prefix} CodeChain View Too High ${suffix}`;
     this.content = `View of the block(${blockNumber}) in CodeChain is ${view.toString(
       10
@@ -36,8 +40,10 @@ export class ViewTooHigh implements CodeChainAlert {
 export class NodeIsSleeping implements CodeChainAlert {
   public readonly title: string;
   public readonly content: string;
+  public readonly level = "warn";
 
   constructor(blockNumber: number, nodeIndices: number[], streak?: number) {
+    const prefix = `[${this.level}][${networkId}][monitor]`;
     const suffix = `${new Date().toISOString()}`;
     this.title = `${prefix} CodeChain Node is Sleeping ${suffix}`;
     if (streak !== undefined) {
@@ -52,8 +58,10 @@ export class NodeIsSleeping implements CodeChainAlert {
 export class NodeRecovered implements CodeChainAlert {
   public readonly title: string;
   public readonly content: string;
+  public readonly level = "info";
 
   constructor(blockNumber: number, nodeIndex: number, sleepStreak: number) {
+    const prefix = `[${this.level}][${networkId}][monitor]`;
     const suffix = `${new Date().toISOString()}`;
     this.title = `${prefix} CodeChain Node has recovered from the problem ${suffix}`;
     this.content = `The node ${nodeIndex} did not precommit from the block ${blockNumber -
@@ -64,8 +72,10 @@ export class NodeRecovered implements CodeChainAlert {
 export class AllNodesAwake implements CodeChainAlert {
   public readonly title: string;
   public readonly content: string;
+  public readonly level = "info";
 
   constructor(blockNumber: number) {
+    const prefix = `[${this.level}][${networkId}][monitor]`;
     const suffix = `${new Date().toISOString()}`;
     this.title = `${prefix} All CodeChain nodes are awake ${suffix}`;
     this.content = `Before the block(${blockNumber}) some nodes did not precommit, but now all nodes are recovered.`;
@@ -75,8 +85,10 @@ export class AllNodesAwake implements CodeChainAlert {
 export class GetBlockFailed implements CodeChainAlert {
   public readonly title: string;
   public readonly content: string;
+  public readonly level = "error";
 
   constructor(blockNumber: number) {
+    const prefix = `[${this.level}][${networkId}][monitor]`;
     const suffix = `${new Date().toISOString()}`;
     this.title = `${prefix} CodeChain failed to get a block ${suffix}`;
     this.content = `RPC chain_getBlockByNumber failed with the best block number ${blockNumber}`;
