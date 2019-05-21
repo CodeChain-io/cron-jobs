4489c06501c7ef1891d784222a2ed8204c779eb5
Park Juhyung <jh@kodebox.io>
2019-05-21 15:46:35 +0900

    [indexer-watcher] Add Indexer Watcher

---
+++ indexer-watcher/src/EmailNotify.ts
@@ -0,0 +1,40 @@
+import * as sgMail from "@sendgrid/mail";
+import { getConfig, haveConfig } from "./util";
+
+export class EmailClient {
+    private from: string;
+
+    public constructor() {
+        if (!haveConfig("sendgrid_api_key")) {
+            if (process.env.NODE_ENV === "production") {
+                throw Error(`SENDGRID_API_KEY not found`);
+            } else {
+                console.log("SENDGRID_API_KEY is null");
+            }
+        }
+        sgMail.setApiKey(getConfig("sendgrid_api_key"));
+        this.from = "no-reply+monitor@devop.codechain.io";
+    }
+
+    public async sendAnnouncement(email: string, title: string, content: string): Promise<void> {
+        // FIXME:
+        await this.send(email, title, content);
+    }
+
+    private async send(to: string, subject: string, text: string) {
+        if (process.env.NODE_ENV === "test") {
+            return;
+        }
+        console.log(` Send an email to ${to}`);
+        console.log(` subject: ${subject}`);
+        console.log(` text: ${text}`);
+
+        const { from } = this;
+        return sgMail.send({
+            from,
+            to,
+            subject,
+            text,
+        });
+    }
+}
---
+++ indexer-watcher/src/Notification.ts
@@ -0,0 +1,114 @@
+import { getConfig } from "./util";
+
+const networkId = getConfig<string>("network_id");
+
+export interface Notification {
+    readonly title: string;
+    readonly content: string;
+    readonly level: "error" | "warn" | "info";
+    readonly date: Date;
+}
+
+export class IndexerPingFailed implements Notification {
+    public readonly title: string;
+    public readonly content: string;
+    public readonly level = "error";
+    public readonly date = new Date();
+
+    public constructor() {
+        const prefix = `[${this.level}][${networkId}][indexer-watcher]`;
+        this.title = `${prefix} Indexer Ping Failed`;
+        this.content = "Indexer is not responding a ping request";
+    }
+}
+
+export class IndexerPingSuccess implements Notification {
+    public readonly title: string;
+    public readonly content: string;
+    public readonly level = "info";
+    public readonly date = new Date();
+
+    public constructor() {
+        const prefix = `[${this.level}][${networkId}][indexer-watcher]`;
+        this.title = `${prefix} Indexer Ping Success`;
+        this.content = "Indexer is responding a ping request";
+    }
+}
+
+export class IndexerSyncTooSlow implements Notification {
+    public readonly title: string;
+    public readonly content: string;
+    public readonly level = "error";
+    public readonly date = new Date();
+
+    public constructor(indexedBlockNumber: number, bestBlockNumber: number) {
+        const prefix = `[${this.level}][${networkId}][indexer-watcher]`;
+        this.title = `${prefix} Indexer Failed to follow the network`;
+        this.content = `Indexer is too slow. Current indexed block number is ${indexedBlockNumber}, but the best block is ${bestBlockNumber}`;
+    }
+}
+
+export class IndexerSyncNormalized implements Notification {
+    public readonly title: string;
+    public readonly content: string;
+    public readonly level = "info";
+    public readonly date = new Date();
+
+    public constructor(indexedBlockNumber: number, bestBlockNumber: number) {
+        const prefix = `[${this.level}][${networkId}][indexer-watcher]`;
+        this.title = `${prefix} Indexer Followed the network`;
+        this.content = `Indexer followed the network. Current indexed block number is ${indexedBlockNumber}, but the best block is ${bestBlockNumber}`;
+    }
+}
+
+export class IndexerCodeChainNotSyncing implements Notification {
+    public readonly title: string;
+    public readonly content: string;
+    public readonly level = "error";
+    public readonly date = new Date();
+
+    public constructor(blockNumber: number) {
+        const prefix = `[${this.level}][${networkId}][indexer-watcher]`;
+        this.title = `${prefix} Indexer's CodeChain is not syncing`;
+        this.content = `The CodeChain Instance used in the indexer's block number is not changed in an hour. Current block number is ${blockNumber}`;
+    }
+}
+
+export class IndexerCodeChainSyncing implements Notification {
+    public readonly title: string;
+    public readonly content: string;
+    public readonly level = "info";
+    public readonly date = new Date();
+
+    public constructor(blockNumber: number) {
+        const prefix = `[${this.level}][${networkId}][indexer-watcher]`;
+        this.title = `${prefix} Indexer's CodeChain is syncing`;
+        this.content = `The CodeChain Instance used in the indexer is syncing well now. Current block number is ${blockNumber}`;
+    }
+}
+
+export class DailyReport implements Notification {
+    public readonly title: string;
+    public readonly content: string;
+    public readonly level = "info";
+    public readonly date = new Date();
+
+    public constructor(firstBlockNumber: number, lastBlockNumber: number) {
+        const prefix = `[${this.level}][${networkId}][indexer-watcher]`;
+        this.title = `${prefix} is working`;
+        this.content = `The Indexer Watcher is working without problem. Yesterday's first block number was ${firstBlockNumber} and last block number was ${lastBlockNumber}.`;
+    }
+}
+
+export class InternalError implements Notification {
+    public readonly title: string;
+    public readonly content: string;
+    public readonly level = "warn";
+    public readonly date = new Date();
+
+    public constructor(error: string) {
+        const prefix = `[${this.level}][${networkId}][indexer-watcher]`;
+        this.title = `${prefix} Indexer Watcher failed`;
+        this.content = `Indexer Watcher failed with this error: ${error}`;
+    }
+}
